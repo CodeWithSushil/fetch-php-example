0efcb42a0282fb0b4a3d90e42b59ada577a74776
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-05-19 20:57:45 +0530

    update to fetchphp/fetch

--- example.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__."/vendor/autoload.php";
-
-use Fetch\Interfaces\Response as ResponseInterface;
-
-// Simple GET request
-$response = fetch('https://example.com');
-echo $users = $response->text();
