4623ecae2918fc2c706659ded83da140dae767e3
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-05-19 12:03:28 +0530

    fetch php update

--- index.php
+++ index.php
@@ -6,7 +6,20 @@ require_once "vendor/autoload.php";
 // Simple GET request
 $response = fetch('https://jsonplaceholder.typicode.com/users');
 $users = $response->json();
+?>
+<table style="width:100%;" border="1" cellpadding="10"  cellspacing="0" >
+<tr>
+<th>Sri No</th>
+<th>Name</th>
+</tr>
+<?php
+$id = 1;
+foreach($users as $key => $user){
+?>
+<tr>
+<td><?= $id++ ?></td>
+<td><?= $user['name'] ?></td>
+</tr>
+<?php } ?>
+</table>
 
-echo '<pre>';
-var_dump($users);
-echo '</pre>';
