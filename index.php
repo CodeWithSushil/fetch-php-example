7440d3f119021e3ba4fa3bbc8f5d0aeeac3dcb68
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-05-19 10:57:02 +0530

    fetch php exrror fixed

--- index.php
+++ index.php
@@ -4,13 +4,5 @@ use Fetch\Interfaces\Response as ResponseInterface;
 require_once "vendor/autoload.php";
 
 // Simple GET request
-$response = fetch('https://api.example.com/users');
-$users = $response->json();
-
-// POST request with JSON body
-$response = fetch('https://api.example.com/users', [
-    'method' => 'POST',
-    'json' => ['name' => 'John Doe', 'email' => 'john@example.com'],
-]);
-
-echo $response->text();
+$response = fetch('https://example.com');
+echo $users = $response->text();
